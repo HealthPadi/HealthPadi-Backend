d1942dde972e67272fd92e5936f7f18c08d98f0f
Emmanuel Adebanjo <adebanjoemmanuel01@gmail.com>
2024-08-14 14:04:42 +0100

    Updated accountService's interface to contain method for signing user with OAuth

--- HealthPadiWebApi/Services/Interfaces/IAccountService.cs
+++ HealthPadiWebApi/Services/Interfaces/IAccountService.cs
@@ -7,5 +7,7 @@ namespace HealthPadiWebApi.Services.Interfaces
     {
         Task<IdentityResult> RegisterUserAsync(RegisterRequestDto registerRequestDto);
         Task<LoginResponseDto> LoginUserAsync(LoginRequestDto loginRequestDto);
+        Task<LoginResponseDto> LoginUserWithGoogleAsync(string email, string firstName, string lastName);
+        
     }
 }
