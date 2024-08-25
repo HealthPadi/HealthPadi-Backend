93ba0597da5908ac40a60b1577798f5b74493aaa
Emmanuel Adebanjo <adebanjoemmanuel01@gmail.com>
2024-08-25 09:21:04 +0100

    Created db migrations and services for weekly execution of HealthFeeds

---
+++ HealthPadiWebApi/Migrations/20240823105551_TaskExecutionLog.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HealthPadiWebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class TaskExecutionLog : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TaskExecutionLogs",
+                columns: table => new
+                {
+                    TaskExecutionLogId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    LastExecutionTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaskExecutionLogs", x => x.TaskExecutionLogId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TaskExecutionLogs");
+        }
+    }
+}
---
+++ HealthPadiWebApi/Services/Interfaces/ITaskExecutionLoggerService.cs
@@ -0,0 +1,10 @@
+﻿using HealthPadiWebApi.Models;
+
+namespace HealthPadiWebApi.Services.Interfaces
+{
+    public interface ITaskExecutionLoggerService
+    {
+        Task<TaskExecutionLog> AddTaskExecutionLog(TaskExecutionLog logToSave);
+        Task<List<TaskExecutionLog>> GetAllTaskExecutionLogs();
+    }
+}
